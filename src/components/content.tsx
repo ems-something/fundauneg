1bcce6c89cc7486f023eb6276e88fc1803bf7eb3
ems-something <emilysotelo2096@gmail.com>
2024-01-25 17:18:44 -0400

    Arreglando animacion, agregando filtro a content

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -11,7 +11,7 @@ const Content = () => {
         </div>
         <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
           <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-            <div className="rounded-lg h-64 overflow-hidden hover:animate-grow">
+            <div className="rounded-lg h-64 overflow-hidden hover:scale-110 hover:brightness-110">
               <img
                 alt="content"
                 className="object-cover object-center h-full w-full"
@@ -42,10 +42,10 @@ const Content = () => {
             </a>
           </div>
           <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-            <div className="rounded-lg h-64 overflow-hidden">
+            <div className="rounded-lg h-64 overflow-hidden hover:scale-110 hover:brightness-110">
               <img
                 alt="content"
-                className="object-cover object-center h-full w-full hover:animate-grow"
+                className="object-cover object-center h-full w-full"
                 src="/images/desarrollo_de_videojuegos.jpg"
               />
             </div>
@@ -73,10 +73,10 @@ const Content = () => {
             </a>
           </div>
           <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-            <div className="rounded-lg h-64 overflow-hidden">
+            <div className="rounded-lg h-64 overflow-hidden hover:scale-110 hover:brightness-110">
               <img
                 alt="content"
-                className="object-cover object-center h-full w-full hover:animate-grow"
+                className="object-cover object-center h-full w-full"
                 src="/images/curso_de_maquillaje.jpg"
               />
             </div>
@@ -104,15 +104,46 @@ const Content = () => {
             </a>
           </div>
           <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-            <div className="rounded-lg h-64 overflow-hidden">
+            <div className="rounded-lg h-64 overflow-hidden hover:scale-110 hover:brightness-110">
               <img
                 alt="content"
-                className="object-cover object-center h-full w-full hover:animate-grow"
+                className="object-cover object-center h-full w-full"
                 src="/images/matematicas_preuniversitarias.jpg"
               />
             </div>
             <h2 className="text-xl font-medium title-font text-white mt-5">
               Matemáticas Preuniversitarias
+            </h2>
+            <p className="text-base leading-relaxed mt-2">
+              Swag shoindegoitch literally meditation subway tile tumblr
+              cold-pressed. Gastropub street art beard dreamcatcher neutra,
+              ethical XOXO lumbersexual.
+            </p>
+            <a className="text-primary inline-flex items-center mt-3">
+              Ver más
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                className="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
+          </div>
+          <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+            <div className="rounded-lg h-64 overflow-hidden hover:scale-110 hover:brightness-110">
+              <img
+                alt="content"
+                className="object-cover object-center h-full w-full"
+                src="/images/diplomado_ecografia_abdominal.jpg"
+              />
+            </div>
+            <h2 className="text-xl font-medium title-font text-white mt-5">
+              Diplomado Ecografía Abdominal
             </h2>
             <p className="text-base leading-relaxed mt-2">
               Swag shoindegoitch literally meditation subway tile tumblr
