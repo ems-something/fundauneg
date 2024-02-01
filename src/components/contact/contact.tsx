05587e0a63505573d36a40ad7949f503bd9bc4f5
ems-something <emilysotelo2096@gmail.com>
2024-02-01 06:01:39 -0400

    Otro pequeño arreglo

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -47,7 +47,7 @@ function Contact() {
         </div>
         <div className="lg:w-1/3 md:w-1/2 md:ml-auto md:py-8 mt-8 md:mt-0">
           <div className="flex flex-col text-center w-full mb-12">
-            <h2 className="text-white text-lg mb-1 font-medium title-font">
+            <h2 className="text-white text-3xl mb-1 font-medium title-font">
               Contáctanos
             </h2>
             <p className="leading-relaxed">
