8865f6305779d790158f32cb449def75921f31e9
ems-something <emilysotelo2096@gmail.com>
2024-03-18 12:07:35 -0400

    Arreglando seccion Nosotros (about-us)

--- src/components/about-us.tsx
+++ src/components/about-us.tsx
@@ -14,32 +14,65 @@ const AboutUs = () => {
             Nosotros
           </h2>
         </div>
-        {aboutUs.map((item, index) => (
-          <div
-            className={`flex items-center sm:items-start mx-auto ${
-              index + 1 < aboutUs.length
-                ? "border-b pb-10 mb-10 border-gray-800"
-                : ""
-            } sm:flex-row flex-col`}
-            key={index}
-          >
-            <div className="sm:w-1/2 sm:h-[32rem] h-80 w-80 sm:mr-10 inline-xflex items-center justify-center shrink-0">
-              <img
-                alt="content"
-                className="object-cover object-top h-full w-full rounded-lg"
-                src={item.imgUrl}
-              />
-            </div>
-            <div className="grow text-center mt-6 sm:mt-0">
-              <h2 className="text-white sm:text-left text-2xl title-font font-medium mb-8">
-                {item.title}
-              </h2>
-              <p className="leading-relaxed text-base sm:text-justify">
-                {item.description}
-              </p>
-            </div>
-          </div>
-        ))}
+        {aboutUs.map((item, index) => {
+          if (index % 2 === 0) {
+            return (
+              <div
+                className={`flex items-center sm:items-start mx-auto ${
+                  index + 1 < aboutUs.length
+                    ? "border-b pb-10 mb-10 border-gray-800"
+                    : ""
+                } sm:flex-row flex-col`}
+                key={index}
+              >
+                <div className="sm:w-1/2 sm:h-[32rem] h-80 w-80 sm:mr-10 inline-xflex items-center justify-center shrink-0">
+                  <img
+                    alt="content"
+                    className="object-cover object-top h-full w-full rounded-lg"
+                    src={item.imgUrl}
+                  />
+                </div>
+                <div className="grow text-center mt-6 sm:mt-0">
+                  <h2 className="text-white sm:text-left text-2xl title-font font-medium mb-8">
+                    {item.title}
+                  </h2>
+                  <p className="leading-relaxed text-base sm:text-justify">
+                    {item.description}
+                  </p>
+                </div>
+              </div>
+            );
+          } else {
+            if (index % 2 !== 0) {
+              return (
+                <div
+                  className={`flex items-center sm:items-start mx-auto ${
+                    index + 1 < aboutUs.length
+                      ? "border-b pb-10 mb-10 border-gray-800"
+                      : ""
+                  } sm:flex-row flex-col`}
+                  key={index}
+                >
+                  <div className="grow text-center mt-6 sm:mt-0">
+                    <h2 className="text-white sm:text-left text-2xl title-font font-medium mb-8">
+                      {item.title}
+                    </h2>
+                    <p className="leading-relaxed text-base sm:text-justify">
+                      {item.description}
+                    </p>
+                  </div>
+                  <div className="sm:w-1/2 sm:h-[32rem] h-80 w-80 order-first sm:order-none sm:ml-10 inline-flex items-center justify-center shrink-0">
+                    <img
+                      alt="content"
+                      className="object-cover object-top h-full w-full rounded-lg"
+                      src={item.imgUrl}
+                    />
+                  </div>
+                </div>
+              );
+            }
+          }
+        })}
       </div>
     </section>
   );
