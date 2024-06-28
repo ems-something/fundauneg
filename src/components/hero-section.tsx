689816c9b22b6df64330ea5b6f0d485dd3fdc1cd
ems-something <emilysotelo2096@gmail.com>
2024-06-28 03:08:25 -0400

    Pequeño ajuste en hero section

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -11,20 +11,20 @@ const HeroSection: React.FC<HeroSectionProps> = ({ open, setOpen }) => {
       className="h-screen w-full text-white bg-top bg-cover bg-blend-overlay bg-fixed bg-black/50 flex flex-col justify-between items-center"
       style={{ backgroundImage: `url(/images/hero-section/hero-section.jpg)` }}
     >
-      <button className="ml-4 mt-4 md:hidden" onClick={setOpen}>
+      <button className="ml-4 mt-4 sm:hidden" onClick={setOpen}>
         <img src="/images/menu.png" alt="" width={30} />
       </button>
       <div className="container h-screen p-6 flex flex-col justify-center items-center sm:p-32  mix-blend-overlay">
         <img
-          className="lg:w-1/5 md:w-1/3 w-64 mb-10"
+          className="lg:w-1/4 md:w-1/3 w-64 mb-10"
           alt="fundauneg logo"
           src="/images/hero-section/fundaUneg-logo.png"
         />
         <div className="text-center lg:w-2/3 w-full">
-          <h1 className="text-5xl sm:text-7xl  mb-5 title-font font-black">
+          <h1 className="text-5xl md:text-7xl  mb-5 title-font font-black">
             FUNDAUNEG
           </h1>
-          <p className="font-light leading-relaxed sm:text-3xl text-xl">
+          <p className="font-light leading-relaxed md:text-3xl text-xl">
             Formación y capacitación con respaldo universitario
           </p>
         </div>
