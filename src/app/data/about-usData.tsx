2c630dff6f472691907017daccb2fc0990254f49
ems-something <emilysotelo2096@gmail.com>
2024-07-20 18:14:52 -0400

    Arreglando sección about us

--- src/app/data/about-usData.tsx
+++ src/app/data/about-usData.tsx
@@ -9,12 +9,12 @@ export const aboutUsData = [
     imgUrl: "/images/about-us/vision.jpg",
     title: "Visión",
     description:
-      'Ser líderes destacados en la prestación de servicios de formación y capacitación respaldados por la Universidad Nacional Experimental de Guayana "Manuel Carlos Piar" (FUNDAUNEG), marcando la pauta a nivel regional, nacional e internacional. Aspiramos a ser reconocidos por nuestras políticas de calidad, oportunidad y competitividad, consolidando alianzas sólidas con el sector empresarial, profesores, estudiantes y la comunidad en general.',
+      "Ser líderes destacados en la prestación de servicios de formación y capacitación respaldados por la Universidad Nacional Experimental de Guayana (UNEG), marcando la pauta a nivel regional, nacional e internacional. Aspiramos a ser reconocidos por nuestras políticas de calidad, oportunidad y competitividad, consolidando alianzas sólidas con el sector empresarial, profesores, estudiantes y la comunidad en general.",
   },
   {
     imgUrl: "/images/about-us/historia.jpg",
     title: "Historia",
     description:
-      'Fundada en 1988, la Fundación de la Universidad Nacional Experimental de Guayana "Manuel Carlos Piar" (FUNDAUNEG) se ha destacado por su compromiso con el crecimiento de la UNEG a nivel regional, nacional e internacional. Establecemos conexiones significativas con el sector empresarial, profesores, estudiantes y la comunidad en general. Nuestros servicios abarcan la capacitación, formación, asistencia técnica y administración, contribuyendo al desarrollo sostenible y competitivo de quienes confían en nosotros.',
+      "Fundada en 1988, la Fundación para la Promoción y Desarrollo de la Universidad Nacional Experimental De Guayana “Manuel Carlos Piar” (FUNDAUNEG) se ha destacado por su compromiso con el crecimiento de la UNEG a nivel regional, nacional e internacional. Establecemos conexiones significativas con el sector empresarial, profesores, estudiantes y la comunidad en general. Nuestros servicios abarcan la capacitación, formación, asistencia técnica y administración, contribuyendo al desarrollo sostenible y competitivo de quienes confían en nosotros.",
   },
 ];
